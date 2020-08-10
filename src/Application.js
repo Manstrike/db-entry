6c9e65e67f3c178c0fce1d2c26d038bf9570df6d
Manstrike <onpu4536b@gmail.com>
2020-08-10 23:06:58 +0300

    minor

--- src/Application.js
+++ src/Application.js
@@ -9,10 +9,6 @@ export class Application {
     }
 
     init() {
-        this._app.get('/', (req, res) => {
-            res.render('./main/main-module');
-        });
-
         this._app.use('/school', this._schoolRouter);
         this._app.use('/teacher', this._teacherRouter);
         this._app.use('/user', this._userRouter);
